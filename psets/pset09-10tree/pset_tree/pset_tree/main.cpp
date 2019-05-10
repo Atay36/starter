2299c467c19b0b3497c56dddb3b29edf2450bc8c
YeeunLee <prayatay36@gmail.com>
2019-05-11 01:23:20 +0900

    ㅎ

---
+++ psets/pset09-10tree/pset_tree/pset_tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pset_tree
+//
+//  Created by YEEUN LEE on 10/05/2019.
+//  Copyright © 2019 YEEUN LEE. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
